dbc706e22aa3598c04d29081fa4f907f6dae0843
herby herado <herby.herado@gmail.com>
2020-03-24 08:56:51 +0700

    Fix: url validation regex

--- src/utils/validateURL.ts
+++ src/utils/validateURL.ts
@@ -1,11 +1,11 @@
 /**
  * Validate string such that it is "url safe"
- * @param {String} url unique id that is going to be stored in firestore
+ * @param {String} url origin address that is to be saved in firestore
  */
 export default (url: String): boolean => {
   if (!url) return false;
   const regex = new RegExp(
-    /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g
+    /^(?:http(s)?:\/\/)?[a-z0-9.-]+(?:\.[a-z0-9.-]+)+[a-z0-9\-._~:/?#[\]@!$&'()*+,;=.]+$/gi
   );
   if (url.match(regex)) {
     return true;
